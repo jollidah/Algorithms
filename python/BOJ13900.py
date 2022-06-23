a24b615ee702f9c5bc7a37b969ccadb66f4df425
jollidah <78254737+jollidah@users.noreply.github.com>
2022-06-23 18:59:32 +0900

    Update BOJ13900.py

--- python/BOJ13900.py
+++ python/BOJ13900.py
@@ -7,3 +7,6 @@ for i in list:
     s -= i              # 모든 리스트의 합을 구해 합에서 앞에 있는 원소를 빼고 남은 합의 값에 곱하는 방법으로 연산 수를 줄인다
     res += s * i        # 연산수를 줄이는게 중요함
 print(res)
+
+
+# https://www.acmicpc.net/problem/13900
