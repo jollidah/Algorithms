4f39238948278b27d9c08269e00539d51f4de9f5
jollidah <78254737+jollidah@users.noreply.github.com>
2022-06-30 10:07:17 +0900

    Create 숫자카드게임.cpp

---
+++ Concept_problem/03_Greedy/숫자카드게임.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    int n, m, tmp = 0, res = 0;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        int minValue = 1e9;     // declare minValue
+        for (int j = 0; j < m; j++)
+        {
+         cin >> tmp;
+        if (tmp < minValue)     // compare input with recent minValue
+        {
+            minValue = tmp;
+        }
+        }
+        if (minValue > res)
+        {
+            res = minValue;
+        }
+    }
+    cout << res;
+}
