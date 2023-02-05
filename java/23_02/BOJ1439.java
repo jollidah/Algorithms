fcd1ef175e5ca7f59a532f0f8a27472af3b7584e
jollidah <78254737+jollidah@users.noreply.github.com>
2023-02-05 13:10:58 +0900

    Update BOJ1439.java

--- java/23_02/BOJ1439.java
+++ java/23_02/BOJ1439.java
@@ -15,3 +15,29 @@ public class Main {
         new Main().solution();
     }
 }
+
+
+import java.io.*;
+//
+//
+//public class Main{
+//    public static void main(String[] args) {
+//        try{
+//            int res = 0;
+//            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            String s1 = br.readLine();
+//            for(int i = 0; i < s1.length() - 1; i++){
+//                if (s1.charAt(i) != s1.charAt(i + 1)){
+//                    res++;
+//                }
+//            }
+//            bw.write(String.valueOf((res+ 1) / 2));
+//            bw.flush();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
+
+// https://www.acmicpc.net/problem/1439
