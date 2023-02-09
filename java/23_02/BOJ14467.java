85fa93750aba48334a9dac2a089d73262f454d2e
jollidah <78254737+jollidah@users.noreply.github.com>
2023-02-09 19:34:31 +0900

    Update BOJ14467.java

--- java/23_02/BOJ14467.java
+++ java/23_02/BOJ14467.java
@@ -1,37 +1,37 @@
-import java.util.*;
+// 소가 길을 건너간 이유 1
+
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[11][1];
-        int cow_sum = 0;
-
-        for(int i = 1; i < 11; i++) {
-            arr[i][0] = -1;
-        }
-
-        StringTokenizer st;
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if(arr[a][0] == -1) {
-                arr[a][0] = b;
-            }
-            else {
-                if(arr[a][0] != b) {
-                    cow_sum++;
-                    arr[a][0] = b;
+public class Main{
+    public static void main(String[] args) {
+        try{
+            int cowNum, pos, res = 0;
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int [] position = new int [11];
+            Arrays.fill(position, -1);
+            for(int i = 0; i < n; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                cowNum = Integer.parseInt(st.nextToken());
+                pos = Integer.parseInt(st.nextToken());
+                if(position[cowNum] == -1){
+                    position[cowNum] = pos;
+                }
+                else if(position[cowNum] != pos){
+                    res++;
+                    position[cowNum] = pos;
                 }
             }
+            bw.write(String.valueOf(res));
+            bw.flush();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-        System.out.println(cow_sum);
     }
 }
+
+// https://www.acmicpc.net/problem/14467
