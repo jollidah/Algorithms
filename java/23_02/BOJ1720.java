22d514a26f011c3da6646563371d8f15f92b5cf2
jollidah <78254737+jollidah@users.noreply.github.com>
2023-02-27 22:01:58 +0900

    Update BOJ1720.java

--- java/23_02/BOJ1720.java
+++ java/23_02/BOJ1720.java
@@ -1,55 +1,35 @@
+// 타일 코드
+
+import java.util.*;
 import java.io.*;
 
-public class n01720 {
+public class Main {
 
-	static int N;
-	static int[] dp = new int[31];
-	static int[][] dp2 = new int[31][31];
-
-	public static void main(String[] args) throws Exception {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		N = Integer.parseInt(br.readLine());
-
-		dp[1] = 1;
-		dp[2] = 3;
-		for (int i = 3; i <= N; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2] * 2;
-		}
-
-		if (N % 2 == 0) {  // N이 짝수일 때
-			dp2[N / 2][N / 2 + 1] = 3;
-			dp2[N / 2 - 1][N / 2 + 2] = 5;
-			
-			int i = N / 2 - 2;
-			int j = N / 2 + 3;
-			while (i >= 1 && j <= N) {
-				dp2[i][j] = dp2[i + 1][j - 1] + dp2[i + 2][j - 2] * 2;
-				i--;
-				j++;
-			}
-		}
-
-		else {  // N이 홀수일 때
-			dp2[N / 2 + 1][N / 2 + 1] = 1;
-			dp2[N / 2][N / 2 + 2] = 1;
-			
-			int i = N / 2 - 1;
-			int j = N / 2 + 3;
-			while (i >= 1 && j <= N) {
-				dp2[i][j] = dp2[i + 1][j - 1] + dp2[i + 2][j - 2] * 2;
-				i--;
-				j++;
-			}
-		}
-
-		int res = (dp[N] - dp2[1][N]) / 2 + dp2[1][N]; // 최종 결과
-
-		bw.write(res + "\n");
-		bw.flush();
-
-	}
-
+    public static void main(String[] args) {
+        try {
+            int n, res = 0;
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            int [] dp = new int [n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            if(n < 2){
+                System.out.println(dp[n]);
+                System.exit(0);
+            }
+            for(int i = 2; i <= n; i++){
+                dp[i] = dp[i - 1] + dp[i - 2] * 2;
+            }
+            if(n % 2 == 1){
+                res = (dp[n] + dp[n / 2]) / 2;
+            } else {
+                res = (dp[n / 2 - 1] * 2 + dp[n / 2] + dp[n]) / 2;
+            }
+            System.out.println(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+// https://www.acmicpc.net/problem/1720
