a83917e852387da2acdf10e701481a6af85504db
jollidah <78254737+jollidah@users.noreply.github.com>
2023-02-27 22:14:13 +0900

    Update BOJ1720.java

--- java/23_02/BOJ1720.java
+++ java/23_02/BOJ1720.java
@@ -2,9 +2,7 @@
 
 import java.util.*;
 import java.io.*;
-
 public class Main {
-
     public static void main(String[] args) {
         try {
             int n, res = 0;
@@ -13,6 +11,7 @@ public class Main {
             int [] dp = new int [n + 1];
             dp[0] = 1;
             dp[1] = 1;
+            // 예외 처리
             if(n < 2){
                 System.out.println(dp[n]);
                 System.exit(0);
@@ -21,8 +20,10 @@ public class Main {
                 dp[i] = dp[i - 1] + dp[i - 2] * 2;
             }
             if(n % 2 == 1){
+                // 홀수는 가운데가 1짜리 블록 고정하기 때문에 2로 나누기만 하면 된다.
                 res = (dp[n] + dp[n / 2]) / 2;
             } else {
+                // 짝수는 두가지 경우로 나눠야 한다 가운데가 세로로된 블록과 그 외의 블록
                 res = (dp[n / 2 - 1] * 2 + dp[n / 2] + dp[n]) / 2;
             }
             System.out.println(res);
