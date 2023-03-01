a274cbdc95b8c64977d96ae5fed3c6ba1e5029f4
jollidah <78254737+jollidah@users.noreply.github.com>
2023-03-01 10:25:08 +0900

    Update BOJ13913.java

--- java/23_03/BOJ13913.java
+++ java/23_03/BOJ13913.java
@@ -1,65 +1,23 @@
  // 숨바꼭질4
  
- //import java.util.*;
-//import java.io.*;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        try {
-//            int n, minV = 1000;
-//            int [] tmp;
-//            HashMap<Integer, Integer> hm = new HashMap<>();
-//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//            StringTokenizer st = new StringTokenizer(br.readLine());
-//            PriorityQueue <int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-//            n = Integer.parseInt(st.nextToken());
-//            pq.add(new int[] {1, 0, 0});
-//            while(!pq.isEmpty()){
-//                tmp = pq.poll();
-//                System.out.println(tmp[0]);
-//                if(tmp[0] == n){
-//                    minV = Math.min(minV, tmp[2]);
-//                }else {
-//                    if (hm.containsKey(tmp[0])){
-//                        if(hm.get(tmp[0]) <= tmp[2]){
-//                            continue;
-//                        }
-//                    } else{
-//                        hm.put(tmp[0], tmp[2]);
-//                    }
-//                    if (tmp[0] < n) {
-//                        pq.add(new int[]{tmp[0] + tmp[1], tmp[1], tmp[2] + 1});
-//                        pq.add(new int[]{tmp[0], tmp[0], tmp[2] + 1});
-//                    }
-//                    pq.add(new int[]{tmp[0] - 1, tmp[1], tmp[2] + 1});
-//                }
-//            }
-//            System.out.println(minV);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            int n, m, tmp;
-            int [] [] dp = new int[2][200000];
-            boolean [] isVisit = new boolean [200000];
+            int n, m, tmp = 0;
+            int [] time = new int[100001];
+            int [] parent = new int[100001];
+            boolean [] isVisit = new boolean [100001];
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
             Queue <Integer> q = new LinkedList<>();
             ArrayList <Integer> al = new ArrayList<>();
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-            Arrays.fill(dp[0],Integer.MAX_VALUE);
+
             q.add(n);
-            dp[0][n] = 0;
-            isVisit[0] = true;
             isVisit[n] = true;
 
             // m이 n보다 작은 경우
@@ -68,51 +26,50 @@ public class Main {
                 while(n >= m){
                     System.out.printf("%d ", n--);
                 }
-            } else{
-                while(!q.isEmpty()){
-                    tmp = q.poll();
-                    if(tmp == m){
-                        System.out.println(dp[0][tmp]);
-                        break;
-                    } else{
-                        if(tmp < m && !isVisit[tmp * 2]){
-                            isVisit[tmp * 2] = true;
-                            dp[0][tmp * 2] = dp[0][tmp] + 1;
-                            dp[1][tmp * 2] = tmp;
-                            q.add(tmp * 2);
-                        }
-                        if(tmp < m && tmp + 1 < 100000 && !isVisit[tmp + 1]){
-                            isVisit[tmp + 1] = true;
-                            dp[0][tmp + 1] = dp[0][tmp] + 1;
-                            dp[1][tmp + 1] = tmp;
-                            q.add(tmp + 1);
-                        }
-                        if(tmp - 1 >= 0 && !isVisit[tmp - 1]){
-                            isVisit[tmp - 1] = true;
-                            dp[0][tmp - 1] = dp[0][tmp] + 1;
-                            dp[1][tmp - 1] = tmp;
-                            q.add(tmp - 1);
-                        }
+                System.exit(0);
+            }
+            
+         // BFS
+            while(!q.isEmpty()){
+                tmp = q.poll();
+                if(tmp == m){
+                    System.out.println(time[tmp]);
+                    break;
+                } else{
+                    if(tmp < m && tmp * 2 <= 100000 && !isVisit[tmp * 2]){
+                        isVisit[tmp * 2] = true;
+                        time[tmp * 2] = time[tmp] + 1;
+                        parent[tmp * 2] = tmp;
+                        q.add(tmp * 2);
+                    }
+                    if(tmp < m && tmp + 1 <= 100000 && !isVisit[tmp + 1]){
+                        isVisit[tmp + 1] = true;
+                        time[tmp + 1] = time[tmp] + 1;
+                        parent[tmp + 1] = tmp;
+                        q.add(tmp + 1);
+                    }
+                    if(tmp - 1 >= 0 && !isVisit[tmp - 1]){
+                        isVisit[tmp - 1] = true;
+                        time[tmp - 1] = time[tmp] + 1;
+                        parent[tmp - 1] = tmp;
+                        q.add(tmp - 1);
                     }
                 }
-                
-                // 출처룰 추적
-                tmp = m;
-                while(tmp != n){
-                    al.add(tmp);
-                    tmp = dp[1][tmp];
-                }
-                System.out.printf("%d ", n);
-                for(int i = al.size() - 1; i >= 0; i --){
-                    System.out.printf("%d ", al.get(i));
-                }
+            }
+            // 출처 추적
+            while(tmp != n){
+                al.add(tmp);
+                tmp = parent[tmp];
+            }
+            System.out.printf("%d ", n);
+            for(int i = al.size() - 1; i >= 0; i --){
+                System.out.printf("%d ", al.get(i));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
 
  
  // https://www.acmicpc.net/problem/13913
