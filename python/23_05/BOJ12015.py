1cda3e14309c3c1fabc2ba1546d9cff7b9cb0d16
unknown <help0304@naver.com>
2023-05-12 22:35:35 +0900

    골드2 / 1h

---
+++ python/23_05/BOJ12015.py
@@ -0,0 +1,27 @@
+# 가장 긴 증가하는 부분 수열 2
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cases = list(map(int, input().split()))
+lis = [0]
+
+for case in cases:
+    if lis[-1]<case:
+        lis.append(case)
+    else:
+        left = 0
+        right = len(lis)
+
+        while left<right:
+            mid = (right+left)//2
+            if lis[mid]<case:
+                left = mid+1
+            else:
+                right = mid
+        lis[right] = case
+
+print(len(lis)-1)
+
+# https://www.acmicpc.net/problem/12015
