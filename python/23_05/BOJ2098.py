85a0151477bf168ce13e36cca9d3860009ce548a
unknown <help0304@naver.com>
2023-05-17 00:49:52 +0900

    골드1 / 2h

--- python/23_05/BOJ2098.py
+++ python/23_05/BOJ2098.py
@@ -3,7 +3,7 @@
 import sys
 
 def DFS(p, path):
-    if dp[p][path] != 0:
+    if dp[p][path] != 1e9:
         return dp[p][path]
     
     if path == (1 << (n - 1)) - 1:
@@ -12,21 +12,19 @@ def DFS(p, path):
         else:
             return 1e9
         
-    min_dist = 1e9
     for i in range(1, n):
         if not cost[p][i]:
             continue
         if path & (1 << i - 1):
             continue
-        min_dist = min(min_dist, cost[p][i] + DFS(i, path | (1 << (i - 1))))
-    dp[p][path] = min_dist
-    return min_dist
+        dp[p][path] = min(dp[p][path], cost[p][i] + DFS(i, path | (1 << (i - 1))))
+    return dp[p][path]
 
 
 input = sys.stdin.readline
 n = int(input().strip())
 cost = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0 for _ in range(1 << n)] for _ in range(n)]
+dp = [[1e9 for _ in range(1 << n)] for _ in range(n)]
 # dp[p][path] -> i에서 path에 들어 있는 점들을 지나 0으로 향하는 최소비용
 
 print(DFS(0, 0))
