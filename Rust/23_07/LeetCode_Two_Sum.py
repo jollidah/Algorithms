02638b649a17a666fcc964ea4287a3b174dc3443
Seungwoo Ko <78254737+jollidah@users.noreply.github.com>
2023-07-01 23:10:26 +0900

    Create LeetCode_Two_Sum.py

---
+++ Rust/23_07/LeetCode_Two_Sum.py
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        nums.sort();
+        let lp : i32 = 0;
+        let rp : i32 = 0;
+        let sum : i32 = 0;
+        let n : i32 = nums.len().try_into().unwrap();
+        while lp < n{
+            lp += 1;
+        }
+    }
+}
