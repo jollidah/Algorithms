bf1253dade99cadf9645cfe2b764002ae60901df
unknown <help0304@naver.com>
2023-07-11 15:55:32 +0900

    골3 / 55m

--- python/23_07/BOJ2457.py
+++ python/23_07/BOJ2457.py
@@ -1,39 +1,32 @@
+# 공주님의 정원
+
 import sys
-from collections import deque
 
 inp = sys.stdin.readline
 n = int(inp().rstrip())
 accumulate = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
-date = accumulate[3] + 1
-target = accumulate[11] + 30
+date = 60
+target = 334
 flowers = []
-tmpMax = 0
+tmpMax = date
 cnt = 0
 for _ in range(n):
     sm, sd, em, ed = map(int, inp().split())
     flowers.append([accumulate[sm] + sd, accumulate[em] + ed])
 flowers.sort()
-# print(date, target)
 # date는 탐색해야 하는 날짜를 포함하고 있음
 for start, end in flowers:
-    # print(start, end)
-    if tmpMax > target:
+    if start > tmpMax:
+        print(0)
         break
     if date < start:
-        if tmpMax == 0:
-            break
-        else:
             cnt += 1
             date = tmpMax
-            # print("cnt", date, target)
-            if date >= start and date <= end:
-                tmpMax = end 
-            else:
-                tmpMax = 0
-    elif date <= end:
-        tmpMax = max(tmpMax, end)
-        
-if date < tmpMax:
-    cnt += 1
-    date = tmpMax
-print(cnt if date > target else 0)
+    tmpMax = max(tmpMax, end)
+    if tmpMax > target:
+         print(cnt + 1)
+         break
+else:
+    print(0)
+
+# https://www.acmicpc.net/problem/2457
