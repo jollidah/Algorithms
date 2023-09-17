5c4f93bbffb68fca05f419e8a52d94b9d2616a41
unknown <help0304@naver.com>
2023-09-17 23:10:56 +0900

    Add Leetcode_1512.py

---
+++ python/23_09/LeetCode_1512.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        repeat = {}
+        num = 0
+        for v in nums:
+            if v in repeat:
+                if repeat[v] == 1:
+                    num += 1
+                else:
+                    num += repeat[v]
+                repeat[v] += 1
+            else:
+                repeat[v] = 1
+        return num
